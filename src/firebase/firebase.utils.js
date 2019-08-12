48fdeae639f73f4541d3749f7d24ddffaa165bae
Aniket Kumar <aniketkumar049@gmail.com>
2019-08-13 01:01:29 +0530

    integrated firebase into app and google auth added

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBn1oW3BacmSooYvwFhprPPTHSokWWmBo4",
+  authDomain: "crwn-db-ced46.firebaseapp.com",
+  databaseURL: "https://crwn-db-ced46.firebaseio.com",
+  projectId: "crwn-db-ced46",
+  storageBucket: "",
+  messagingSenderId: "1007461301396",
+  appId: "1:1007461301396:web:d8fbead2e7030886"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
